8604fef404a058c24c8684d3109ff8093de25224
Ibrahim Rabbani <israbbani@gmail.com>
2018-06-13 16:13:21 -0700

    added hosts conf file

--- shared/constants.sh
+++ shared/constants.sh
@@ -4,3 +4,12 @@ hadoop_out=hadoop-2.7.6
 jdk=jdk-8u172-linux-x64.tar.gz
 jdk_out=jdk1.8.0_172
 
+# cluster nodes
+primary=h-primary
+secondary_a=h-secondary-a
+secondary_b=h-secondary-b
+
+# user 
+user=hadoop
+pass=hadoop
+home=/home/hadoop
